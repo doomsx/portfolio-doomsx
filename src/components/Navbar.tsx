3f79d4ea995d7c098dd575654907ef414c8f7e57
Doomsx <127058006+doomsx@users.noreply.github.com>
2024-10-19 20:43:10 +0800

    navbar and hero

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,22 @@
 
 function Navbar() {
     return (
-        <header className="sticky top-0 w-full bg-dark flex justify-between px-3 py-5">
+        <header className="sticky top-0 w-full bg-transparent backdrop-blur-md flex justify-between items-center px-3 py-3 border-b border-white">
             <h1 className="text-accent font-bold text-xl">Doms</h1>
             <nav>
                 <ul className="text-light flex items-center gap-3">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
+                    <a href="#Home" className="navLink">
+                        <li>Home</li>
+                    </a>
+                    <a href="#About" className="navLink">
+                        <li>About</li>
+                    </a>
+                    <a href="#Projects" className="navLink">
+                        <li>Projects</li>
+                    </a>
+                    <a href="#Contact" className="navLink">
+                        <li>Contact</li>
+                    </a>
                 </ul>
             </nav>
         </header>
