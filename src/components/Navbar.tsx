1994156b345c8c33c6f1632da2cb64c488bc347d
Doomsx <127058006+doomsx@users.noreply.github.com>
2024-10-19 10:01:44 +0800

    navbar and tailwind

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
 
 function Navbar() {
     return (
-        <header>Navbar</header>
+        <header className="sticky top-0 w-full bg-dark flex justify-between px-3 py-5">
+            <h1 className="text-accent font-bold text-xl">Doms</h1>
+            <nav>
+                <ul className="text-light flex items-center gap-3">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        dark: "#0a192f",
+        light: "#e3e3e8",
+        accent: "#64ffda",
+      },
+      textColor: {
+        light: "#e3e3e8",
+        dark: "#0a192f",
+        accent: "#64ffda",
+      },
+    },
   },
   plugins: [],
 };
