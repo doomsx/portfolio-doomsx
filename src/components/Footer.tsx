a512a71664a5ba4b1290e52b0ef6d6fab4ada2a8
Doms <dlmagat08@proton.me>
2025-01-14 22:44:03 +0800

    make the year dynamic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,14 @@
+
 function Footer() {
-  return (
-    <footer className="h-20 border-t border-[#64ffda]">
-      <div className="flex justify-center items-center h-full">
-        Dominique &copy; 2024
-      </div>
-    </footer>
-  );
+    const currentYear = new Date().getFullYear()
+
+    return (
+        <footer className="h-20 border-t border-[#64ffda]">
+            <div className="flex justify-center items-center h-full">
+                Dominique &copy; {currentYear}
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
