5d4bc12647540d2e37d99eeeabc2e94d626e2def
Doms <dlmagat08@proton.me>
2025-03-22 20:07:50 +0800

    adding projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,6 +3,9 @@ function Projects() {
     return (
         <section className="h-screen flex items-center justify-center" id="Projects">
             <h1 className="text-5xl font-black text-accent">Projects</h1>
+            <div>
+                <h2>Weather App</h2>
+            </div>
         </section>
     )
 }
