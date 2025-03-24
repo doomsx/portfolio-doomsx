53f4ec8dbf500d2872c37c3d2dc551beb9457650
Doms <dlmagat08@proton.me>
2025-03-24 20:31:40 +0800

    project layout

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,17 @@
 
 function Projects() {
     return (
-        <section className="h-screen flex items-center justify-center" id="Projects">
+        <section className="h-screen flex items-center justify-center flex-col" id="Projects">
             <h1 className="text-5xl font-black text-accent">Projects</h1>
-            <div>
-                <h2>Weather App</h2>
+            <div className="mt-10 flex gap-3 items-center justify-center">
+                <div>
+                    Image
+                </div>
+                <div>
+                    <h2>Weather App</h2>
+                    <p>description</p>
+                    <p>Technology used</p>
+                </div>
             </div>
         </section>
     )
