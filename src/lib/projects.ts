b3bdc1c95f761aa9d49d7bb7ae49b0eb1e6c7b1b
Doms <dlmagat08@proton.me>
2025-05-08 12:21:41 +0800

    local

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,13 +1,23 @@
-import weather from "/weather.png"
+import noPhoto from "/no-photo.jpg"
+import nextjs from "/nextdotjs.svg"
+import tailwind from "/tailwind.svg"
 
 export const projects = [
     {
-        img: weather,
+        img: "/weather.png",
         title: "Weather App",
         description: "A simple weather application built with Next.js, TypeScript, and Tailwind CSS. It fetches real-time weather data from the OpenWeather API based on user input, displaying key details like temperature, humidity, and weather conditions. Designed for a smooth and responsive user experience.",
-        skills: ["/nextdotjs.svg", "/tailwind.svg"],
+        skills: [nextjs, tailwind],
         githubLink: "https://github.com/doomsx/weather-app",
         websiteLink: "https://domss-weather-app.vercel.app/"
+    },
+    {
+        img: noPhoto,
+        title: "MovieDB",
+        description: "Simple IMDB clone",
+        skills: [nextjs, tailwind],
+        githubLink: "#",
+        websiteLink: "#"
     }
 ]
 
